c178f67848950303086a3406b886e57a92662843
mm7259 <mm7259@nyu.edu>
2017-01-22 10:33:58 -0500

    Add files via upload

---
+++ pi_interface.py
@@ -0,0 +1,39 @@
+# sensors: 
+# - RFID for association of LCL + container
+# - GPS for detecting LCL destination 
+# - A counter to start association/ loading session
+
+#EXIT DATA
+#the GPS location is simulated using a timer
+#when the GPS location is reached  
+
+#GPS is emulated using timer 
+#RFID tag is emulated using button press 
+#each loading session is represented by the counter
+
+from random import randint
+import requests
+import time 
+
+listOfLCL = [1, 2, 3]
+tagurl = 'https://lcl-enterprises.herokuapp.com/lcls/register' 
+arrivalurl = 'https://lcl-enterprises.herokuapp.com/lcls/arrival'
+
+counter = 0; 
+
+while counter <= 3: 
+	randLCL = randint(0, 2)
+	tagEmulate = input("Press a to emulate the tag: ")
+	if tagEmulate == 'a': 
+		r = requests.post(tagurl, data = {'container_id' : "GLFU2814428", 'lcl_id' : listOfLCL[randLCL]}) #container ID is unique to each microprocessor 
+	counter = counter + 1 #simulates each loading session 
+
+start_time = time.time()
+time.sleep(3)
+journey_time = time.time() - start_time 
+
+if journey_time >= 3: #after 3 seconds have been elapsed
+	arrival = requests.post(arrivalurl, data = {'container_id' : "GLFU2814428"} )
+
+
+
